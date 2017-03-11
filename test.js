051e203f248b5c3f39df7b3b2d9f0ad20d46c7dd
TheWispy <will0humph@gmail.com>
2017-03-11 16:04:14 +0000

    Station placement

--- test.js
+++ test.js
@@ -1,42 +1,75 @@
-var renderer = new THREE.WebGLRenderer();
-renderer.setPixelRatio( window.devicePixelRatio);
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+var camera;
+var scene;
+var renderer;
+var controls;
+
+init();
+animate();
 
 //Scene initialisation
-var scene = new THREE.Scene();
-var aspectRatio = window.innerWidth/window.innerHeight
-var camera = new THREE.PerspectiveCamera( 75, aspectRatio, 0.1, 1000 );
-var controls = new THREE.OrbitControls( camera );
+function init(){
+	//Create scene
+	scene = new THREE.Scene();
 
-var light = new THREE.AmbientLight( 0x404040 );
-scene.add(light);
+	//Create camera
+	var aspectRatio = window.innerWidth/window.innerHeight
+	camera = new THREE.PerspectiveCamera( 75, aspectRatio, 0.1, 1000 );
+	camera.position.set(0, 10, 20);
+	camera.rotation.x = -0.2;
 
-var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5);
-directionalLight.position.set(0, 15, 0);
-scene.add( directionalLight );
+	//Add scene elements
+	addSceneElements();
 
-var boxMaterial = new THREE.MeshPhongMaterial({ color: 0x880044});
-var floorMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff});
-var floor = new THREE.Mesh( new THREE.BoxGeometry(50, 1, 50), floorMaterial)
-var cube = new THREE.Mesh( new THREE.BoxGeometry(10,10,10), boxMaterial );
-scene.add( cube );
-scene.add( floor );
+	//Add lights
+	addLights();
 
-camera.position.z = 20;
-camera.rotation.x = -0.2;
-camera.position.y = 10;
+	//renderer
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio);
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
 
-function render() {
-	requestAnimationFrame( render );
-	controls.update();
-	renderer.render(scene, camera);
-};
+	//Add controls
+	controls = new THREE.OrbitControls(camera);
+}
+
+function addLights(){
+	var light = new THREE.AmbientLight( 0x404040 );
+	scene.add(light);
+
+	var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5);
+	directionalLight.position.set(2, 15, 0);
+	scene.add( directionalLight );
+}
+
+function addSceneElements(){
+	var stations = [[10, 20], [0, 5], [-15, 15], [10, -10]];
+	var stationMat = new THREE.MeshPhongMaterial({ color: 0xe0f0f0});
+	var boxMat = new THREE.MeshPhongMaterial({ color: 0x880044});
+	var floorMat = new THREE.MeshPhongMaterial({ color: 0xffffff});
+
+
+	var floor = new THREE.Mesh( new THREE.BoxGeometry(50, 1, 50), floorMat);
+	var cube = new THREE.Mesh( new THREE.BoxGeometry(10,10,10), boxMat );
+
+	for(var i=0; i<stations.length; i++){
+		var stationMesh = new THREE.Mesh( new THREE.BoxGeometry(5,5,5), stationMat);
+		scene.add(stationMesh);
+		stationMesh.position.set(stations[i][0], 0, stations[i][1]);
+	}
+	scene.add(cube);
+	scene.add(floor);
+
+}
+
+function animate() {
+    renderer.render( scene, camera );
+    requestAnimationFrame( animate );
+    controls.update();
+}
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
-};
-
-render();
+}
