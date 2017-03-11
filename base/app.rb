a4f83abdcb3ecb5276d0f46f14753905991050a1
Charlie Lee <dev@charlielee.uk>
2017-03-11 22:45:51 +0000

    Create train groupings

--- base/app.rb
+++ base/app.rb
@@ -6,24 +6,85 @@ require 'http'
 APP_ID = 98792731;
 APP_KEY = "5674da74eee9936d936bdb41c2b1b7c8";
 
+set :bind, "0.0.0.0"
+
 # Website Index page (root directory)
-get '/' do
-	make_train_objects
-	# @json = make_train_objects
-	# erb :index
+get '/index.json' do
+	
+
+	check_stations_on_route ["SHF", "CHD", "DBY", "TAM", "BHM"]
+	
 end
 
-def get_departures
-	response = HTTP.get("https://transportapi.com/v3/uk/train/station/SHF/live.json?app_id=#{APP_ID}&app_key=#{APP_KEY}&darwin=false&train_status=passenger").body
-	return JSON.parse(response)
-end
+def check_stations_on_route route_array
+	trainsList = []
 
-def make_train_objects
-	rawData = get_departures
-	curDepartures = rawData["departures"]["all"]
+	# Make train objects for each station in route_array
+	route_array.each do |curStation|
+		departures = make_train_objects curStation
 
-	curDepartures.each do |departure|
-		puts departure["arrival_time"]
+		
+		departures.each do |departure|
+			# New station for route
+			newStation = {
+				:name => curStation,
+				:estimate_mins => departure[:estimate_mins]
+			}
+
+			# Check station seen before
+			seenBefore = false
+			trainsList.each do |train|
+				if (train[:uid] == departure[:uid])
+					# Add station and time to existing item if seen before
+					puts "I'VE SEEN THIS FUCKING THING BEFOREEEEEEEEEEE"
+					seenBefore = true
+
+					train[:stations].push(newStation)
+				end
+			end
+
+			# Add to array if never seen before
+			if !seenBefore
+				trainsList.push({
+					:uid => departure[:uid],
+					:stations => [newStation]
+				})
+			end
+
+			#puts trainsList[trainsList.length-1]
+		end
 	end
+
+	#puts trainsList
+	output = trainsList
+	return JSON.pretty_generate(output)
+end
+
+def make_train_objects station_name
+	rawData = get_departures station_name
+	curDepartures = rawData["departures"]["all"]
+	trainsList = []
+
+	# Make trainsList object for each departure
+	curDepartures.each_with_index do |departure, i|
+		train = {
+			:service => departure["service"],
+			:uid => departure["train_uid"],
+			:operator => departure["operator"],
+			:origin => departure["origin_name"],
+			:destination => departure["destination_name"],
+			:estimate_mins => departure["best_departure_estimate_mins"],
+			:arrival_time => departure["expected_arrival_time"],
+			:departure_time => departure["expected_departure_time"]
+		}
+
+		trainsList[i] = train
+
+	end
+	return trainsList
+end
+
+def get_departures station_name
+	return JSON.parse(HTTP.get("https://transportapi.com/v3/uk/train/station/#{station_name}/live.json?app_id=#{APP_ID}&app_key=#{APP_KEY}&darwin=false&train_status=passenger").body)
 end
 
