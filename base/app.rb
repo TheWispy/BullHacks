6d3defb496d67c027487ed171bebdf171cdb5763
EC2 Default User <ec2-user@ip-172-26-7-160.ec2.internal>
2017-03-12 10:46:43 +0000

    Adjust sql

--- base/app.rb
+++ base/app.rb
@@ -91,7 +91,7 @@ post '/schedule.json' do
 	db = SQLite3::Database.new('trains2.db')
 	db.results_as_hash = true
 
-	sql = "SELECT codes.description, schedule_location.arrival, schedule_location.departure, schedule_location.pass, schedule.train_uid, codes.crs, schedule.stp FROM schedule_location INNER JOIN schedule ON schedule.id = schedule_location.train_id INNER JOIN codes ON schedule_location.tiploc_code = codes.tiploc WHERE schedule.train_uid = :uid AND schedule.stp = (SELECT MIN(schedule.stp) FROM schedule WHERE schedule.train_uid = :uid) AND description NOT NULL AND pass IS NULL"
+	sql = "SELECT codes.description, schedule_location.arrival, schedule_location.departure, schedule_location.pass, schedule.train_uid, codes.crs, schedule.stp FROM schedule_location INNER JOIN schedule ON schedule.id = schedule_location.train_id INNER JOIN codes ON schedule_location.tiploc_code = codes.tiploc WHERE schedule.train_uid = :uid AND schedule.stp = (SELECT MIN(schedule.stp) FROM schedule WHERE schedule.train_uid = :uid) AND description NOT NULL AND pass IS NULLAND arrival NOT LIKE '%H%' GROUP BY codes.description ORDER BY arrival"
 
 	result = db.execute(sql, {'uid' => train_uid})
 
