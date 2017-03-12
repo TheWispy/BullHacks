853d16598446ad9ab01d2f52941251c650cf1f82
TheWispy <will0humph@gmail.com>
2017-03-12 00:06:39 +0000

    Possible sky and animation functionality.

--- visualiser.js
+++ visualiser.js
@@ -4,6 +4,7 @@ var renderer;
 var controls;
 var stations = [[10, 20], [0, 5], [-15, 15], [10, -10]];
 var train;
+var textureLoader = new THREE.TextureLoader();
 
 init();
 animate();
@@ -11,7 +12,7 @@ animate();
 function init(){
 	//Create scene
 	scene = new THREE.Scene();
-
+	scene.background = new THREE.Color( 0xffa0a0 );
 	//Create camera
 	var aspectRatio = window.innerWidth/window.innerHeight
 	camera = new THREE.PerspectiveCamera( 75, aspectRatio, 0.1, 1000 );
@@ -27,11 +28,15 @@ function init(){
 	//renderer
 	renderer = new THREE.WebGLRenderer();
 	renderer.setPixelRatio( window.devicePixelRatio);
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setSize( window.innerWidth, window.innerHeight);
+	renderer.shadowMapEnabled = true;
+	renderer.shadowMapSoft = true;
 	document.body.appendChild( renderer.domElement );
 
 	//Add controls
 	controls = new THREE.OrbitControls(camera);
+
+	beginTrain(1, 2, 0.5);
 }
 
 function addLights(){
@@ -40,8 +45,10 @@ function addLights(){
 
 	var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5);
 	directionalLight.position.set(2, 15, 0);
+	directionalLight.rotation.x = 0.2;
 	scene.add( directionalLight );
 }
+
 
 function lineBetween(array, i, j){
 	geometry = new THREE.Geometry(100, 10);
@@ -52,21 +59,27 @@ function lineBetween(array, i, j){
 	scene.add(line);
 }
 
-function train(to, from, factor){
-	this.to = to;
-	this.from = from;
-	this.factor = 0;
-}
-
 function beginTrain(i, j, factor){
     var trainMat = new THREE.MeshPhongMaterial({ color: 0xd42525});
-    train = new THREE.Mesh( new THREE.BoxGeometry(1, 1, 4));
+    train = new THREE.Mesh( new THREE.BoxGeometry(1, 6, 4), trainMat);
     scene.add(train);
     train.position.set(stations[i][0], 1, stations[i][1]);
 
     var dx = stations[j][0] - train.position.x;
     var dz = stations[j][1] - train.position.z;
-	var tween = new TWEEN.Tween(train.position).to({ x: train.position.x+dx*factor, y: mesh.position.y, z: train.position.z+dz*factor }, time).start();
+
+	var position = train.position;
+	var target = { x: train.position.x+dx*factor, y: train.position.y, z: train.position.z+dz*factor };
+	var tween = new TWEEN.Tween(position).to(target, 10000);
+	tween.onUpdate(function(){
+		if (train.position.x == stations[j][0] && train.position.z == stations[j][1]){
+			scene.remove(train);
+			console.log("Train nuked");
+		}
+    	train.position.x = position.x;
+		train.rotation.y = Math.atan(dx/dz);
+    	train.position.z = position.z;});
+	tween.start();
 }
 
 function addSceneElements(){
